13b068905ba86b829ef1f793d834043abe5d0b7a
Igor <igorberaldomo@gmai.com>
2024-10-23 09:38:36 -0300

    1.0

--- server.go
+++ server.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	client := http.Client{Timeout:time.Microsecond*200 }
+	resp, err := client.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err :=io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}
