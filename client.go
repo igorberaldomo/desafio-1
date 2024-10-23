eabf26bd05f0910f976308d495c1649e1ea54a7c
Igor <igorberaldomo@gmai.com>
2024-10-23 10:07:08 -0300

    1.1

--- client.go
+++ client.go
@@ -1,1 +1,26 @@
 package main
+
+import(
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func client(){
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*300)
+	defer cancel()
+
+	// client executa o request para o server
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+}
--- server.go
+++ server.go
@@ -4,16 +4,26 @@ import (
 	"io"
 	"net/http"
 	"time"
+	"context"
 )
 
 func main() {
-	client := http.Client{Timeout:time.Microsecond*200 }
-	resp, err := client.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+	// server disponibiliza as informções da API
+	http.HandleFunc("/cotacao", handleCotacao)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleCotacao(w http.ResponseWriter, r *http.Request) {
+	// faz a requisição para a API
+	ctxreq, cancel := context.WithTimeout(r.Context(), time.Millisecond*200)
+	defer cancel()
+	// server consome a API
+	req, err := http.NewRequestWithContext(ctxreq, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	res , err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(err)
 	}
-	defer resp.Body.Close()
-	body, err :=io.ReadAll(resp.Body)
+	defer res.Body.Close()
+	body, err :=io.ReadAll(res.Body)
 	if err != nil {
 		panic(err)
 	}
